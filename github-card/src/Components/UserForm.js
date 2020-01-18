72433eeb0110f755b9ffabed44626ff9b7de00be
Brandon Franks <bgfranks106@icloud.com>
2020-01-17 21:47:59 -0500

    styled the form

--- github-card/src/Components/UserForm.js
+++ github-card/src/Components/UserForm.js
@@ -1,9 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
+import "./hover.css";
+
 const FormContainer = styled.div`
   padding-bottom: 20px;
-  color: #f3f3f3;
+
+  input {
+    background-color: #2e294e;
+    border: 0;
+    padding: 6px;
+    font-size: 1rem;
+    width: 240px;
+    outline: 0;
+    color: #f3f3f3;
+    border-bottom: 1px solid #f3f3f3;
+    margin-right: 8px;
+  }
+
+  button {
+    margin-left: 10px;
+    height: 30px;
+    width: 80px;
+    color: #f3f3f3;
+    border: 1px solid #ff9505;
+    background: #ff9505;
+    font-size: 1.1rem;
+  }
 `;
 
 const UserForm = ({ value, handleUserTextChange, handleUserFetch }) => {
@@ -11,7 +34,9 @@ const UserForm = ({ value, handleUserTextChange, handleUserFetch }) => {
     <FormContainer>
       <form>
         <input type="text" value={value} onChange={handleUserTextChange} />
-        <button onClick={handleUserFetch}>Search</button>
+        <button className="hvr-float-shadow" onClick={handleUserFetch}>
+          Search
+        </button>
       </form>
     </FormContainer>
   );
